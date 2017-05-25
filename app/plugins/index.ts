92d1eb3195a107fba4eba92819301d232f065305
Aaron Sky <asky@sapient.com>
2017-05-24 20:59:31 -0400

    Added option for loading plugins from multiple directories

--- app/plugins/index.ts
+++ app/plugins/index.ts
@@ -1,6 +1,7 @@
 import * as glob from 'glob';
 import * as Router from 'koa-router';
 import * as _ from 'lodash';
+import * as os from 'os';
 import * as path from 'path';
 
 import PluginModel from './model';
@@ -8,18 +9,25 @@ import PluginService from './service';
 
 namespace Plugins {
     let plugins: PluginService[] = loadPlugins();
-    export function loadPlugins(): PluginService[] {
-        const dirs = glob.sync('./beeswax-plugin-*/', {
-            cwd: __dirname
-        });
-        return dirs.map(dir => {
-            const pluginName = dir.slice(2, -1);
-            const pluginPath = path.resolve(__dirname, dir);
-            console.log('Loading plugin', pluginName, 'at', pluginPath);
-            let cls = require(pluginPath);
-            cls = cls.default || cls;
-            return new cls();
-        });
+    export function loadPlugins(overridePath: string = null): PluginService[] {
+        const paths = {
+            builtIn: path.resolve(__dirname),
+            default: path.resolve(os.homedir(), '.beeswax', 'plugins'),
+            personal: overridePath
+        };
+        return Object.keys(paths)
+            .map((key) => paths[key])
+            .filter(path => path)
+            .map((path) => glob.sync('./beeswax-plugin-*/', { cwd: path }).map(loadPluginAtPath.bind(null, path)))
+            .reduce((acc, plugins) => [...acc, ...plugins], []);
+    }
+    function loadPluginAtPath(parentDir: string, dirName: string): PluginService {
+        const pluginName = dirName.slice(2, -1);
+        const pluginPath = path.resolve(parentDir, dirName);
+        console.log('Loading plugin', pluginName, 'at', pluginPath);
+        let cls = require(pluginPath);
+        cls = cls.default || cls;
+        return new cls();
     }
     export function routers(app, handle: (req, res) => Promise<any>): Router[] {
         return plugins.map(plugin => plugin.router(app, handle));
